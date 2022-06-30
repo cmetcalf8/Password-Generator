732ee01b2a88a2fbbc641e434956b5e770047607
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-06-29 21:26:03 -0600

    adds functions

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 // Assignment code here
 
+var passwordLength = 8; 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = getPrompts();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function passwordCriteria() {
+  var generate = prompt("Select desired password criteria.");
+  if (generate == null) {
+    document.getElementById("button").innerHTML = 
+    "Thank you.";
+  }
+}
+
+function getPrompts() { 
+  
+}
+
+  if(passwordLength <8 || passwordLength >128) {
+    alert("Please input between 8 and 128 characters.")
+    return;
+  } else {
+    // do passwordy things
+  }
