ae672f2478d1252c43c6ef97ab2a9acb972f25a5
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-07-02 14:41:13 -0700

    adds new functions

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Assignment code here
-
 var passwordLength = 8; 
+var randomArray = [];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,28 +13,43 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = getPrompts();
-  var passwordText = document.querySelector("#password");
+  var password = passwordCriteria();
 
-  passwordText.value = password;
+  if (password) {
+    var newPassword = generatePassword();
+    var passwordText = document.querySelector("#password");
 
+  passwordText.value = newPassword;
+}
+}
+
+function generatePassword() {
+  var password = ""; 
+  for(var i =  0; i < characterLength; I++) {
+    var randomIndex = Math.floor(Math.random() * randomArray.length);
+    password = password + randomArray[randomIndex];
+  }
 }
 
 function passwordCriteria() {
-  var generate = prompt("Select desired password criteria.");
-  if (generate == null) {
-    document.getElementById("button").innerHTML = 
-    "Thank you.";
-  }
-}
-
-function getPrompts() { 
-  
-}
-
-  if(passwordLength <8 || passwordLength >128) {
-    alert("Please input between 8 and 128 characters.")
+  randomArray = [];
+  var generate = prompt("Enter the length of your password between 8 and 128 characters.");
+  if(generate <8 || generate >128) {
+    alert("Please input a number between 8 and 128 characters.")
     return;
   } else {
-    // do passwordy things
+    confirm("Do you want lowercase letters in your password?");
+    randomArray = randomArray.concat(lowercase);
+
+    confirm("Do you want uppercase letters in your password?");
+    randomArray = randomArray.concat(uppercase);
+    
+    confirm("Do you want numbers in your password?");
+    randomArray = randomArray.concat(numeric);
+    
+    confirm("Do you want special characters in your password?");
+    randomArray = randomArray.concat(special);
+    
+    return password;
   }
+}
